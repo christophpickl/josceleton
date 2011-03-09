f6451f399fefcd00e6b5197f657a7b2a52b35fcf
christoph_pickl <christoph.pickl@gmail.com>
2011-03-09 17:33:38 +0000

    0000010 josceleton facade changes

--- artifacts/josceleton-reactor/trunk/josceleton/src/main/java/net/sf/josceleton/josceleton/Josceleton.java
+++ artifacts/josceleton-reactor/trunk/josceleton/src/main/java/net/sf/josceleton/josceleton/Josceleton.java
@@ -6,12 +6,13 @@ import net.sf.josceleton.core.api.entity.message.JointMessage;
 import net.sf.josceleton.core.api.entity.message.UserMessage;
 
 import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 
 /**
  * @since 0.2
  */
-public final class Josceleton /*statically implements JosceletonFacade */ {
+public final class Josceleton /* statically implements JosceletonFacade */ {
 	
 	public static void main(final String[] args) {
 		final Connection connection = Josceleton.openConnection();
@@ -28,9 +29,7 @@ public final class Josceleton /*statically implements JosceletonFacade */ {
 	}
 	
 	private static final JosceletonFacade FACADE =
-		new JosceletonFacadeImpl(
-			Guice.createInjector(
-				new JosceletonGuiceModule()));
+		new JosceletonFacadeImpl(Josceleton.newGuiceInjector());
 	
 	private Josceleton() {
 		// non instantiable, as just static methods available
@@ -48,5 +47,14 @@ public final class Josceleton /*statically implements JosceletonFacade */ {
 	 */
 	public static Connection openConnectionOnPort(final int port) {
 		return FACADE.openConnectionOnPort(port);
+	}
+	
+	/**
+	 * Handy utility method only, creating a new "instance realm" and should only be invoked once.
+	 * 
+	 * @since 0.2
+	 */
+	public static Injector newGuiceInjector() {
+		return Guice.createInjector(new JosceletonGuiceModule());
 	}
 }
--- artifacts/josceleton-reactor/trunk/josceleton/src/main/java/net/sf/josceleton/josceleton/JosceletonGuiceModule.java
+++ artifacts/josceleton-reactor/trunk/josceleton/src/main/java/net/sf/josceleton/josceleton/JosceletonGuiceModule.java
@@ -9,15 +9,22 @@ import net.sf.josceleton.core.impl.entity.message.JosceletonCoreImplEntityMessag
 import com.google.inject.AbstractModule;
 
 /**
+ * Contains all submodules necessary to work with Josceleton.
+ * 
+ * Internally just uses the {@link #install(com.google.inject.Module)} method to accomplish this.
+ * 
  * @since 0.2
  */
 public class JosceletonGuiceModule
-	extends AbstractModule { // NO extends PrivateModule; we dont want to artificially limit access to internals 
+	extends AbstractModule { // NO extends PrivateModule ... we dont want to artificially limit access to internals 
 	
 	@Override protected final void configure() {
+		
 		this.install(new JosceletonCommonsReflectGuiceModule());
+		
 		this.install(new JosceletonCoreImplEntityMessageGuiceModule());
 		this.install(new JosceletonCoreImplEntityGuiceModule());
+		
 		this.install(new JosceletonConnectionImplOscGuiceModule());
 		this.install(new JosceletonConnectionImplGuiceModule());
 	}
