5b0be08078f0785eda31736e74e63dc7daeed7de
christoph_pickl <christoph.pickl@gmail.com>
2011-03-09 00:18:33 +0000

    fixed bug concerning generic ClassAdapter parameter and guice ctd.

--- artifacts/josceleton-reactor/trunk/connection-impl/src/test/java/net/sf/josceleton/connection/impl/osc/JosceletonConnectionImplOscGuiceModuleTest.java
+++ artifacts/josceleton-reactor/trunk/connection-impl/src/test/java/net/sf/josceleton/connection/impl/osc/JosceletonConnectionImplOscGuiceModuleTest.java
@@ -1,6 +1,11 @@
 package net.sf.josceleton.connection.impl.osc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import net.sf.josceleton.commons.test.AbstractGuiceModuleTest;
+
+import org.testng.annotations.Test;
 
 import com.google.inject.Module;
 
@@ -9,5 +14,9 @@ public class JosceletonConnectionImplOscGuiceModuleTest extends AbstractGuiceMod
 	@Override protected final Module createTestee() {
 		return new JosceletonConnectionImplOscGuiceModule();
 	}
-
+	
+	@Test public final void providerExecutesProperyAndReturnsValue() {
+		assertThat(new JosceletonConnectionImplOscGuiceModule().getOscPortInClassAdapter(), not(nullValue()));
+	}
+	
 }
