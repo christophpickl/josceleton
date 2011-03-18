5feafec049d58788b98f40bead253eaa6955553e
christoph_pickl <christoph.pickl@gmail.com>
2011-03-18 10:40:53 +0000

    mainly site

--- project_management/mvn_scripts/includes/configuration.sh
+++ project_management/mvn_scripts/includes/configuration.sh
@@ -5,25 +5,25 @@
 DEBUG=1
 
 # assumes you have checked out the projects in the <BASE_PATH>.
-BASE_PATH="/Users/phudy/_dev/pulse"
+BASE_PATH="/Users/phudy/_dev/josc"
 
 # each project is then specified with its folder name in <ARTIFACTS_PATH_ARRAY>.
-ARTIFACTS_PATH_ARRAY=( "josceleton-corporate-pom" "josceleton-poms-reactor" "josceleton-reactor" )
+# ARTIFACTS_PATH_ARRAY=( "corporate-pom" "josceleton-reactor" )
 
 
-ARTIFACTS_SINGLE_PATH_ARRY=( \
-	\
-	"josceleton-corporate-pom" \
-	\
-	"josceleton-poms-reactor/guice-dependencies" \
-	"josceleton-poms-reactor/josceleton-checkstyle-config" \
-	"josceleton-poms-reactor/java-abstract-pom" \
-	"josceleton-poms-reactor/java-pom" \
-	\
-	"josceleton-reactor/commons" \
-	"josceleton-reactor/core-api" \
-	"josceleton-reactor/core-impl" \
-	"josceleton-reactor/connection-api" \
-	"josceleton-reactor/connection-impl" \
-	"josceleton-reactor/josceleton" \
-	)
+#ARTIFACTS_SINGLE_PATH_ARRY=( \
+#	\
+#	"josceleton-corporate-pom" \
+#	\
+#	"josceleton-poms-reactor/guice-dependencies" \
+#	"josceleton-poms-reactor/josceleton-checkstyle-config" \
+#	"josceleton-poms-reactor/java-abstract-pom" \
+#	"josceleton-poms-reactor/java-pom" \
+#	\
+#	"josceleton-reactor/commons" \
+#	"josceleton-reactor/core-api" \
+#	"josceleton-reactor/core-impl" \
+#	"josceleton-reactor/connection-api" \
+#	"josceleton-reactor/connection-impl" \
+#	"josceleton-reactor/josceleton" \
+#	)
