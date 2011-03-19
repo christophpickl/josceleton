efde54d164119b567336bfad0102a4c29ad01f0e
christoph_pickl <christoph.pickl@gmail.com>
2011-03-19 04:20:23 +0000

    ops

--- artifact/josceleton/core-api/tags/core-api-0.4/src/test/java/net/sf/josceleton/core/api/entity/message/AbstractMessageTest.java
+++
@@ -1,8 +0,0 @@
-package net.sf.josceleton.core.api.entity.message;
-
-import net.sf.josceleton.commons.test.AbstractEqualsTest;
-
-public abstract class AbstractMessageTest<M extends GenericMessage>
-	extends AbstractEqualsTest<M> {
-	// marker interface/class, whatever ...
-}
