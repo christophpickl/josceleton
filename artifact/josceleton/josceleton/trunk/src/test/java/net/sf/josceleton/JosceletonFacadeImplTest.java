148d004b2205c59f9779ea8cb91d71f63aa43a2c
christoph_pickl <christoph.pickl@gmail.com>
2011-03-20 16:52:48 +0000

    moved net.sf.josceleton.josceleton stuff to net.sf.josceleton

---
+++ artifact/josceleton/josceleton/trunk/src/test/java/net/sf/josceleton/JosceletonFacadeImplTest.java
@@ -0,0 +1,54 @@
+package net.sf.josceleton;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import net.sf.josceleton.commons.test.jmock.AbstractMockeryTest;
+import net.sf.josceleton.connection.api.Connection;
+import net.sf.josceleton.connection.api.Connector;
+
+import org.hamcrest.Matchers;
+import org.jmock.Expectations;
+import org.testng.annotations.Test;
+
+import com.google.inject.Injector;
+
+public class JosceletonFacadeImplTest extends AbstractMockeryTest {
+	
+	@Test public final void openConnectionSuccessfully() {
+		final Connection expectedConnection = this.mock(Connection.class);
+		final JosceletonFacade facade = this.createTestee(expectedConnection, null);
+		final Connection actualConnection = facade.openConnection();
+		
+		assertThat(actualConnection, Matchers.is(expectedConnection));
+	}
+	
+	@Test public final void openConnectionOnPortSuccessfully() {
+		final Connection expectedConnection = this.mock(Connection.class);
+		final Integer expectedPort = Integer.valueOf(42);
+		final JosceletonFacade facade = this.createTestee(expectedConnection, expectedPort );
+		final Connection actualConnection = facade.openConnectionOnPort(expectedPort.intValue());
+		
+		assertThat(actualConnection, Matchers.is(expectedConnection));
+	}
+	
+	private JosceletonFacade createTestee(final Connection expectedConnection, final Integer onPort) {
+		final Connector mockedConnector = this.mock(Connector.class);
+		
+		this.checking(new Expectations() { {
+			if(onPort != null) {
+				oneOf(mockedConnector).openConnectionOnPort(onPort.intValue());
+			} else {
+				oneOf(mockedConnector).openConnection();
+			}
+			will(returnValue(expectedConnection));
+		}});
+
+		final Injector mockedInjector = this.mock(Injector.class);
+		this.checking(new Expectations() { {
+			oneOf(mockedInjector).getInstance(Connector.class);
+			will(returnValue(mockedConnector));
+		}});
+		
+		return new JosceletonFacadeImpl(mockedInjector);
+	}
+	
+}
---
+++ artifact/josceleton/josceleton/trunk/src/test/java/net/sf/josceleton/JosceletonGuiceModuleTest.java
@@ -0,0 +1,13 @@
+package net.sf.josceleton;
+
+import net.sf.josceleton.commons.test.AbstractModuleTest;
+
+import com.google.inject.Module;
+
+public class JosceletonGuiceModuleTest extends AbstractModuleTest {
+
+	@Override protected final Module createTestee() {
+		return new JosceletonGuiceModule();
+	}
+
+}
