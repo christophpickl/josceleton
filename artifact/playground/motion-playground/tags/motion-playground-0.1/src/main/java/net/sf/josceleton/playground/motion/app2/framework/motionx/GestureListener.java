146224f90e2e6949f47546f8cddf301840447791
christoph_pickl <christoph.pickl@gmail.com>
2011-03-24 02:57:37 +0000

    revert

--- artifact/playground/motion-playground/tags/motion-playground-0.1/src/main/java/net/sf/josceleton/playground/motion/app2/framework/motionx/GestureListener.java
+++
@@ -1,9 +0,0 @@
-package net.sf.josceleton.playground.motion.app2.framework.motionx;
-
-import net.sf.josceleton.core.api.async.Listener;
-
-public interface GestureListener<R extends GestureResult> extends Listener {
-
-	void onGestureDetected(R resutl);
-
-}
--- artifact/playground/motion-playground/tags/motion-playground-0.1/src/main/java/net/sf/josceleton/playground/motion/app2/framework/page/PageManagerListener.java
+++
@@ -1,9 +0,0 @@
-package net.sf.josceleton.playground.motion.app2.framework.page;
-
-import net.sf.josceleton.core.api.async.Listener;
-
-public interface PageManagerListener extends Listener {
-
-	void onQuit();
-
-}
