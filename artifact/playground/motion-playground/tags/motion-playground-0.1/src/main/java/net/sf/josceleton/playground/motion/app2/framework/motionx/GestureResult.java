e7db1aeecb63cf6b118c9640db2d504ea815c19e
christoph_pickl <christoph.pickl@gmail.com>
2011-03-24 02:46:46 +0000

    revert

--- artifact/playground/motion-playground/tags/motion-playground-0.1/src/main/java/net/sf/josceleton/playground/motion/app2/framework/motionx/GestureResult.java
+++
@@ -1,5 +0,0 @@
-package net.sf.josceleton.playground.motion.app2.framework.motionx;
-
-public interface GestureResult {
-
-}
--- artifact/playground/motion-playground/tags/motion-playground-0.1/src/main/java/net/sf/josceleton/playground/motion/app2/framework/view/component/ButtonListener.java
+++
@@ -1,9 +0,0 @@
-package net.sf.josceleton.playground.motion.app2.framework.view.component;
-
-import net.sf.josceleton.core.api.async.Listener;
-
-public interface ButtonListener extends Listener {
-	
-	void onClicked(Button source);
-	
-}
