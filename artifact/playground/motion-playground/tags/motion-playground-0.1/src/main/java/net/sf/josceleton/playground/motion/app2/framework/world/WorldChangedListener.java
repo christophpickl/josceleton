05d4083f456c3deb3e2cf0235a9f5e268f02b1f8
christoph_pickl <christoph.pickl@gmail.com>
2011-03-24 03:03:39 +0000

    revert

--- artifact/playground/motion-playground/tags/motion-playground-0.1/src/main/java/net/sf/josceleton/playground/motion/app2/framework/world/WorldChangedListener.java
+++
@@ -1,9 +0,0 @@
-package net.sf.josceleton.playground.motion.app2.framework.world;
-
-import net.sf.josceleton.core.api.async.Listener;
-
-public interface WorldChangedListener extends Listener {
-	
-	void onUpdated(WorldSnapshot world);
-	
-}
